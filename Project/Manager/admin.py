a1c2c7a90a14e5ed844c5a8df836818e77d0440a
ISSAM <129122326+BEXXISSAM@users.noreply.github.com>
2024-05-10 12:01:03 +0100

    Delete Project directory

--- Project/Manager/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Employee, Leaves, Department, holidays
-
-
-admin.site.register(Employee)
-admin.site.register(Leaves)
-admin.site.register(Department)
-admin.site.register(holidays)
